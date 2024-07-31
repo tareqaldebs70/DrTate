9c1ed002ba22f68e0b6637bfcdcf9f821d76aac6
tareqaldebs70 <tareqaldebs70@gmail.com>
2024-07-31 23:22:05 +0000

    third initialize

---
+++ includes/common/config.hxx
@@ -0,0 +1,43 @@
+
+
+
+
+///===--- common/config.hxx - Project config hearder -------*- C++ -*-------===///
+//                                                                             ///
+// Part of the DoctorTate Project.                                             ///
+///                                                                            ///
+//===-----------------------------------------------------------------------===///
+///                                                                            ///
+/// \file                                                                      ///
+/// This file contains the macros of the DoctorTate Project config             ///
+///                                                                            ///
+///===----------------------------------------------------------------------===///
+
+
+///==---------------------------------------------------------------------===///
+///DoctorTate It's a platformer game                                         ///
+///Copyright (C) 2024  TareqAldebs                                           ///
+///==---------------------------------------------------------------------===///
+///This program is free software: you can redistribute it and/or modify      ///
+///it under the terms of the GNU General Public License as published by      ///
+///the Free Software Foundation, either version 3 of the License, or         ///
+///(at your option) any later version.                                       ///
+///==---------------------------------------------------------------------===///
+///This program is distributed in the hope that it will be useful,           ///
+///but WITHOUT ANY WARRANTY; without even the implied warranty of            ///
+///MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             ///
+///GNU General Public License for more details.                              ///
+///==---------------------------------------------------------------------===///
+///You should have received a copy of the GNU General Public License         ///
+///along with this program.  If not, see:                                    ///
+/// https://www.gnu.org/licenses/gpl-3.0.en.html.                            ///
+///==---------------------------------------------------------------------===///
+
+
+
+
+#define PROJECT_NAME          "DoctorTate"
+#define PROJECT_VERSION_MAJOR 0
+#define PROJECT_VERSION_MINOR 0
+#define PROJECT_VERSION_PATCH 0
+#define PROJECT_VERSION_TWEAK 1
---
+++ sources/main/main.cxx
@@ -0,0 +1,9 @@
+
+#include"common/config.hxx"
+#include<iostream>
+
+int main()
+{
+    std::cout << PROJECT_VERSION_MAJOR << "\n";
+    return 0;
+}
